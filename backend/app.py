42cb0546f9206f006fd9678481e513b25c3fbf10
TabasumGit <tabassum.fidoo@gmail.com>
2024-10-04 10:01:29 +0100

    Removed uncommented code and added path correctly

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import json
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend', static_url_path='')  # Static folder set to frontend
 CORS(app)
 data_file = 'data.json'
 
@@ -13,12 +13,16 @@ try:
 except json.JSONDecodeError as e:
     print(f"Error decoding JSON: {e}")
 
+@app.route('/')
+def home():
+    return send_from_directory('../frontend', 'index.html')  # Serve index.html directly
+
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
     prefix = request.args.get('prefix', '').lower()
     albums = []
     songs = []
-    names=[]
+    names = []
 
     # Search for albums and songs matching the prefix
     for artist in data:
@@ -31,7 +35,7 @@ def autocomplete():
                 if song['title'].lower().startswith(prefix):
                     songs.append(song['title'])
 
-    return jsonify({'albums': albums, 'songs': songs, 'names':names})
+    return jsonify({'albums': albums, 'songs': songs, 'names': names})
 
 if __name__ == '__main__':
     app.run(debug=True)
